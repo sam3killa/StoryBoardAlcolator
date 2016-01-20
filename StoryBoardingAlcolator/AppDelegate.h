9e86b51f0a501d681c989c5712b61d42c5657e78
sam3killa <samuels.shih@gmail.com>
2016-01-20 11:49:47 -0800

    Initial Commit

---
+++ StoryBoardingAlcolator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StoryBoardingAlcolator
+//
+//  Created by Samuel Shih on 1/20/16.
+//  Copyright © 2016 Samuel Shih. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StoryBoardingAlcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StoryBoardingAlcolator
+//
+//  Created by Samuel Shih on 1/20/16.
+//  Copyright © 2016 Samuel Shih. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
