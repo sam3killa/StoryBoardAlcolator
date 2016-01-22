3204ec53dd128823362c645065417988ff5b67a6
sam3killa <samuels.shih@gmail.com>
2016-01-22 13:53:36 -0800

    Finished Navigation Checkpoint

---
+++ StoryBoardingAlcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  StoryBoardingAlcolator
+//
+//  Created by Samuel Shih on 1/20/16.
+//  Copyright © 2016 Samuel Shih. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
